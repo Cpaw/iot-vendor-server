027da670d322bec67bfe29d4863d344aec74aa63
u10e10 <uplus.e10@gmail.com>
2016-09-26 19:18:28 +0900

    Refactoring variable cursor.

--- vendor-server.py
+++ vendor-server.py
@@ -12,6 +12,9 @@ server.bind((bind_ip, bind_port))
 server.listen(5)
 print("[*] Listening on %s:%d" % (bind_ip, bind_port))
 
+connect = sqlite3.connect("test.db")
+cursor = connect.cursor()
+
 def handle_client(client_socket):
     request = client_socket.recv(1024)
     print("===Received\n%s" % request) # bytes
@@ -21,7 +24,6 @@ def handle_client(client_socket):
     print("===url[0]: %s" % url[0])
     if url[0] == '':
         print("===Menu")
-        cursor = sqlite3.connect("test.db").cursor()
         cursor.execute("select * from menu")
         data = { "itemList": [ { 'name':  row[0], 'price': row[1], 'value': row[2] } for row in cursor.fetchall()] }
         client_socket.send(json.dumps(data).encode())
