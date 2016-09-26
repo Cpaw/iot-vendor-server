4ce51ff1834542df3709a06da294dfe1ef042d48
u10e10 <uplus.e10@gmail.com>
2016-09-26 20:25:10 +0900

    Move variable definition under function definition

--- vendor-server.py
+++ vendor-server.py
@@ -3,18 +3,6 @@ import threading
 import json
 import sqlite3
 
-bind_ip = ''
-bind_port = 2000
-gLoopStatus = True
-
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((bind_ip, bind_port))
-server.listen(5)
-print("[*] Listening on %s:%d" % (bind_ip, bind_port))
-
-connect = sqlite3.connect("test.db")
-cursor = connect.cursor()
-
 def handle_client(client_socket):
     request = client_socket.recv(1024)
     print("===Received\n%s" % request) # bytes
@@ -46,10 +34,24 @@ def handle_client(client_socket):
 
     client_socket.close()
 
+bind_ip = ''
+bind_port = 2000
+gLoopStatus = True
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((bind_ip, bind_port))
+server.listen(5)
+print("[*] Listening on %s:%d" % (bind_ip, bind_port))
+
+connect = sqlite3.connect("test.db")
+cursor = connect.cursor()
+
 while gLoopStatus:
     client, addr = server.accept()
     print("\n===[*] Accepted connection from: %s:%d" % (addr[0], addr[1]))
     client_handler = threading.Thread(target=handle_client, args=(client, ))
     client_handler.start()
 
+cursor.close()
 connect.close()
+server.close()
